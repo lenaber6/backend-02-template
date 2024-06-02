15a93ff1f30d094931bb4b59d21b0436603f900c
lenaber6 <lenaber6@mail.ru>
2024-06-02 23:51:03 +0300

    listen

--- src/modules/app.js
+++ src/modules/app.js
@@ -2,5 +2,9 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
 
-    
+
+});
+
+server.listen(3003, () => {
+    console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 })
